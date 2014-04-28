afcde40bec22f3fea727c83a5dd78dd3a912250c
Aastha <aastha.tangri@trantorinc.com>
2014-04-28 13:20:25 +0530

    application

---
+++ app/mailers/admin_mailer.rb
@@ -0,0 +1,16 @@
+class AdminMailer < ActionMailer::Base
+
+
+  def hr_document(recipient_email,email_subject,email_body,email_attachment,sender_email)
+
+
+    attachments[File.basename(email_attachment)] = File.read(email_attachment)
+   
+    mail(:from => sender_email,
+      :to => recipient_email,
+      :cc => "nidhi.ayri@trantorinc.com",
+      :subject => email_subject,
+      :body => email_body)
+  end
+
+end
